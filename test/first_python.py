432ba167d8b016b8f426590e51162694a1a21d65
haitao <690181477@qq.com>
2019-05-09 16:10:25 +0800

    add some function

--- test/first_python.py
+++ test/first_python.py
@@ -1,4 +1,12 @@
+def login(username, password):
+    if username == "123456" and password == "123456":
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
     print("hello python")
     print("username")
     print("new file")
+    print(login('123456', '123456'))
